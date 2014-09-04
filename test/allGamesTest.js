4ceb36ddfa08f653763460b5dcbda043098eebc3
Michael Schippert <michael.schippert@gmail.com>
2014-09-03 17:22:35 -0700

    Add tests for daysGames function

--- test/allGamesTest.js
+++ test/allGamesTest.js
@@ -1,8 +1,10 @@
+var _ = require( 'underscore' );
 var expect = require( 'chai' ).expect;
 var nock = require( 'nock' );
 var allGames = require( '../src/allGames.js' );
 var helpers = require( '../src/helpers.js' );
 var miniData = require( '../stubs/miniscoreboard.js' );
+var daysGamesData = require( '../stubs/daysGamesData.js' );
 
 describe( "listGameIds Function", function() {
   var testDate = helpers.makeDate();
@@ -22,6 +24,35 @@ describe( "listGameIds Function", function() {
       list.forEach( function( gid, index ) {
         expect( gid ).to.equal( miniData.gidList[ index ] );
       })
+    })
+  });
+
+});
+
+describe( "daysGames Function", function() {
+  var testDate = helpers.makeDate();
+  var path;
+  var test;
+  var options = {
+    masterScoreboard: 'master_scoreboard.json',
+    miniScoreboard: 'miniscoreboard.json'
+  }
+
+  afterEach( function() {
+    nock.cleanAll();
+  });
+
+  it( "Should respond with requested data", function() {
+    _.each( options, function( ext, func ) {
+      path = testDate + ext;
+      var mlbApi = nock( 'http://gd2.mlb.com' )
+        .get( '/components/game/mlb/' +
+          path )
+        .reply( 200, daysGamesData[ func ] );
+      allGames[ func ]().then( function( data ) {
+        test = data;
+        expect( test ).to.deep.equal( daysGamesData[ func ].data.games );
+      });
     })
   });
 
