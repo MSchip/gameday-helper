5efe04c7e351391d840fe26c57d5c28352b04e9d
Michael Schippert <michael.schippert@gmail.com>
2014-09-05 09:19:19 -0700

    Modify single game to create date based on gid.

--- src/singleGame.js
+++ src/singleGame.js
@@ -1,7 +1,11 @@
 var helpers = require( './helpers.js' );
 var Promise = require( 'bluebird' );
 
-var gameData = function( type, gid, date ) {
+var gameData = function( type, gid ) {
+  var year = parseInt( gid.slice( 4, 8 ) );
+  var month = parseInt( gid.slice( 9, 11 ) ) - 1;
+  var day = parseInt( gid.slice( 12, 14 ) );
+  var date = new Date( year, month, day );
   var findUrl = helpers.makeUrl( helpers.makeDate( date ) + gid + type );
   
   return new Promise( function( resolve, reject ) {
